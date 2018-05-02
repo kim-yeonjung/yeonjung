4b74dc135dafbaabb6a2d3e5c5b391d547860f3c
YeonJung Kim <duswnd25@gmail.com>
2018-05-03 07:57:36 +0900

    change router

--- src/routes/main/kitchen.js
+++ src/routes/main/kitchen.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const postList = require('../../../resource/post_list.json');
 const title = "YeonJung Kim - 쭝식당";
 
-router.get("/post/:post_index", function (req, res, next) {
+router.get("/", function (req, res, next) {
     res.render('basic_layout', {title: title, page: 'food_post', data: postList});
+});
+
+router.get("/post/:post_index", function (req, res, next) {
+    res.render('basic_layout', {title: title, page: 'food_post', data: postList[req.params.post_index]});
 });
 
 router.get("/recent_post", function (req, res, next) {
