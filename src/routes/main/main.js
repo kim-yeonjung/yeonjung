32fc4382ee3d46322a61725f4eb47cfdfbcc52fe
YeonJung Kim <duswnd25@gmail.com>
2018-05-03 04:33:29 +0900

    remove

--- src/routes/main/main.js
+++ src/routes/main/main.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const createError = require('http-errors');
-const postData = require('./../../data/post_data');
 
+//const createError = require('http-errors');
 //next(createError(404));
 
 router.get("/", function (req, res, next) {
@@ -11,23 +10,6 @@ router.get("/", function (req, res, next) {
 
 router.get("/portfolio", function (req, res, next) {
     res.render('portfolio', {data: ""});
-});
-
-router.get("/post/:post_id", function (req, res, next) {
-    //req.params.post_id
-    postData.getPost(req.params.post_index, function (result, error) {
-        if (error)
-            createError(404);
-        return res.status(200).json({
-            count: postData.getRecent()
-        });
-    });
-});
-
-router.get("/recent_post", function (req, res, next) {
-    return res.status(200).json({
-        count: postData.getRecent()
-    });
 });
 
 module.exports = router;
