bd7428f9cd31794c4ed5891b809f781edf27a619
YeonJung Kim <duswnd25@gmail.com>
2018-05-04 00:43:58 +0900

    오류 수정

--- src/routes/api/kakao.js
+++ src/routes/api/kakao.js
@@ -50,8 +50,8 @@ router.post("/message", (req, res) => {
             response = responseGenerator.textWithSubMenuType('모든 오픈 내역 입니다.', openList);
             break;
         default:
-            if (content.contains('차 오픈')) {
-                let index = parseInt(content.split(' '))[0] - 1;
+            if (content.includes('차 오픈')) {
+                let index = parseInt(content.split(" ")[0]) - 1;
                 response = responseGenerator.photoType(postList[index].description, postList[index].post[0].img_src, (postList[index].index + 1) + '차 오픈', postList[index].url);
             } else {
                 response = responseGenerator.textType('잘못된 요청 입니다.');
