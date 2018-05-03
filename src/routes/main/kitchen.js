a99234efcd15f9aad7cced04456abfcd93888490
YeonJung Kim <duswnd25@gmail.com>
2018-05-03 14:39:43 +0900

    이동

--- src/routes/main/kitchen.js
+++ src/routes/main/kitchen.js
@@ -4,11 +4,11 @@ const postList = require('../../../resource/post_list.json');
 const title = "YeonJung Kim - 쭝식당";
 
 router.get("/", function (req, res, next) {
-    res.render('basic_layout', {title: title, page: 'food_list', data: postList});
+    res.render('basic_layout', {title: title, page: 'kitchen/food_list', data: postList});
 });
 
 router.post("/food_view", function (req, res, next) {
-    res.render('food_view', {
+    res.render('kitchen/food_view', {
         title: title + " " + req.body.index + "차 오픈",
         data: postList[req.body.index]
     });
