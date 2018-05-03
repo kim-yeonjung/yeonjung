45d70d0f800e26aaaf461ba947897d2f4f41d684
YeonJung Kim <duswnd25@gmail.com>
2018-05-03 15:41:32 +0900

    test

--- src/routes/main/kitchen.js
+++ src/routes/main/kitchen.js
@@ -7,7 +7,7 @@ router.get("/", function (req, res, next) {
     res.render('basic_layout', {title: title, page: 'kitchen/food_list', data: postList});
 });
 
-router.post("/food_view", function (req, res, next) {
+router.get("/food_view", function (req, res, next) {
     res.render('kitchen/food_view', {
         title: title + " " + req.body.index + "차 오픈",
         data: postList[req.body.index]
