6a4181a8c7d0e17ae566615ec69bd0deb4b7e606
YeonJung Kim <duswnd25@gmail.com>
2018-05-10 07:50:15 +0900

    영화

--- src/routes/main/movie.js
+++ src/routes/main/movie.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const postList = require('../../../resource/movie_list.json');
+const movieList = require('../../../resource/movie/movie_list.json');
+
+const title = "YeonJung Kim - 영화";
 
 router.get("/", function (req, res) {
-    res.render('basic_layout', {title: title, page: 'book/book_list', data: postList});
+    res.render('basic_layout', {title: title, page: 'movie/movie_list', data: movieList, active_index: 4});
 });
 
 module.exports = router;
