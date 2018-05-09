439415a910d6cb2c681b1189ca3d9e6a5614c75a
YeonJung Kim <duswnd25@gmail.com>
2018-05-10 07:08:19 +0900

    it non it 분리

--- src/routes/main/book.js
+++ src/routes/main/book.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const postList = require('../../../resource/book/book_list.json');
+const itBookList = require('../../../resource/book/book_list_it.json');
+const elseBookList = require('../../../resource/book/book_list_else.json');
 
 const title = "YeonJung Kim - 읽은 도서";
 
 router.get("/", function (req, res) {
-    res.render('basic_layout', {title: title, page: 'book/book_list', data: postList});
+    res.render('basic_layout', {
+        title: title,
+        page: 'book/book_list',
+        data: req.param('category').includes("it") ? itBookList : elseBookList
+    });
 });
 
 module.exports = router;
