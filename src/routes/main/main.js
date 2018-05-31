a7fe04ec590c41859b6d1c3975b6a5eca05319a8
YeonJung Kim <duswnd25@gmail.com>
2018-05-31 11:29:30 +0900

    To Upper

--- src/routes/main/main.js
+++ src/routes/main/main.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = express.Router();
+const Express = require('express');
+const Router = Express.Router();
 
-router.get("/", function (req, res) {
+Router.get("/", function (req, res) {
     res.render('basic_layout', {title: 'YeonJung Kim', page: 'index', active_index: 0});
 });
 
-router.get("/portfolio", function (req, res) {
+Router.get("/portfolio", function (req, res) {
     res.render('portfolio');
 });
 
-module.exports = router;
+module.exports = Router;
