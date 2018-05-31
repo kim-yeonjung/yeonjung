de2881be9c8acf162199429707e88d0fbe05698d
YeonJung Kim <duswnd25@gmail.com>
2018-05-31 11:43:54 +0900

    Revert "test"
    This reverts commit cb3adebaaabb8bf2b4b3c85e1ca1ab5ff48d0bdf.

--- app.js
+++ app.js
@@ -41,8 +41,10 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res) {
+    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
     res.status(err.status || 500);
     res.render('basic_layout', {title: "YeonJung Kim", page: 'error', active_index: -1});
 });
