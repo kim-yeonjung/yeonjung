cd999464e9a0a86ef1f224c24bbe921e167271b7
YeonJung Kim <duswnd25@gmail.com>
2018-06-01 12:30:39 +0900

    add github calendar

--- src/routes/main/main.js
+++ src/routes/main/main.js
@@ -1,8 +1,25 @@
 const Express = require('express');
 const Router = Express.Router();
+const Rp = require('request-promise');
+const Cheerio = require('cheerio');
 
 Router.get("/", function (req, res) {
-    res.render('basic_layout', {title: 'YeonJung Kim', page: 'index', active_index: 0});
+    let options = {
+        uri: 'https://github.com/users/duswnd25/contributions',
+        transform: function (body) {
+            return Cheerio.load(body);
+        }
+    };
+
+    Rp(options).then(function ($) {
+        res.render('basic_layout', {
+            title: 'YeonJung Kim',
+            page: 'index',
+            active_index: 0,
+            github_contribution: $('svg').eq(0)
+        });
+    }).catch(function (err) {
+    });
 });
 
 Router.get("/portfolio", function (req, res) {
