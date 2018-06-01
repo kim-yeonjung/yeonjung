55e20042207d48e6ac4e85b1b8760121feea6a39
YeonJung Kim <duswnd25@gmail.com>
2018-06-01 12:49:12 +0900

    add puppeteer

--- src/routes/main/main.js
+++ src/routes/main/main.js
@@ -2,10 +2,21 @@ const Express = require('express');
 const Router = Express.Router();
 const Rp = require('request-promise');
 const Cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 Router.get("/", function (req, res) {
+
+    puppeteer.launch()
+        .then((browser) => {
+            return browser.newPage()
+                .then((page) => {
+                    return page.goto('https://github.com/')
+                        .then(() => page.screenshot({path: 'github.png'}))
+                })
+                .then(() => browser.close());
+        });
     let options = {
-        uri: 'https://github.com/users/duswnd25/contributions',
+        uri: 'https://github.com/duswnd25/',
         transform: function (body) {
             return Cheerio.load(body);
         }
@@ -16,7 +27,7 @@ Router.get("/", function (req, res) {
             title: 'YeonJung Kim',
             page: 'index',
             active_index: 0,
-            github_contribution: $('svg').eq(0)
+            github_contribution: $('div.mb-5.border.border-gray-dark.rounded-1.py-2').eq(0)
         });
     }).catch(function (err) {
     });
