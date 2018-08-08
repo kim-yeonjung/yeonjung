4cccbdbdeb029937f72ef5ac73970d32c7b58988
duswnd25 <duswnd25@gmail.com>
2018-08-09 06:18:30 +0900

    404

--- app.js
+++ app.js
@@ -31,19 +31,7 @@ app.use("/", require("./src/routes/main/main.js"));
 app.use("/kitchen", require("./src/routes/main/kitchen.js"));
 app.use("/api/kitchen/kakao", require("./src/routes/api/kakao"));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.get('*', function (req, res) {
+    res.render('error');
 });
-
-// error handler
-app.use(function (err, req, res) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('basic_layout', {title: "YeonJung Kim", page: 'error', active_index: -1});
-});
-
 module.exports = app;
