48548056834ff6dd5b9578c3fb4833015026471f
duswnd25 <duswnd25@gmail.com>
2018-08-09 21:21:49 +0900

    분리작업

--- resource/portfolio/portfolio.js
+++ resource/portfolio/portfolio.js
@@ -12,7 +12,7 @@ let data = {
         "location": "광주광역시 / 경기도 부천",
         "email": "duswnd25@gmail.com",
         "website": "https://yeonjung.herokuapp.com",
-        "blog": "",
+        "blog": "https://kyeonjung.tistory.com",
         "nationality": "Korea Republic of"
     },
     "language": [
@@ -66,48 +66,7 @@ let data = {
             "end": "2011"
         }
     ],
-    "career": [
-        {
-            "company": "Betdon",
-            "role": "외주 개발자",
-            "description": "에고소드 게임 서버, 관리자용 Dashboard 프로그램 개발 및 유지보수",
-            "start": "2018.2",
-            "end": "",
-            "tech": [
-                "Node.js", "HTML", "CSS", "heroku", "mongoDB", "Parse.com", "Bootstrap"
-            ]
-        },
-        {
-            "company": "GH International MERKEN",
-            "role": "쇼핑몰 유지 보수 및 전산 담당",
-            "description": "쇼핑몰 플랫폼 이전 및 유지/보수 각종 전산작업 담당",
-            "start": "2017.10",
-            "end": "",
-            "tech": [
-                "HTML", "CSS", "Javascript", "cafe24"
-            ]
-        },
-        {
-            "company": "제 11 기계화 보병사단",
-            "role": "대한민국 육군 병장 만기 전역",
-            "description": "중대 본부 정비반 장비수리부속공구보급병",
-            "start": "2015.8",
-            "end": "2017.5",
-            "tech": [
-                "장비수리부속공구보급", "특급전사"
-            ]
-        },
-        {
-            "company": "웰대리",
-            "role": "메인 개발자",
-            "description": "사회적 기업, 협동조합 모델을 통한 지속 발전 가능한 상생형 대리운전 시스템의 고객용, 기사용 Client 및 모니터링, 관리 프로그램 개발",
-            "start": "2014",
-            "end": "2015",
-            "tech": [
-                "Android", "HTML", "CSS", "Javascript", "Parse.com", "Bootstrap"
-            ]
-        }
-    ],
+    "career": [],
     "project": [],
     "license": [
         {
@@ -131,109 +90,38 @@ let data = {
             "date": "2006"
         }
     ],
-    "award": [
-        {
-            "title": "학술연구장학",
-            "grade": "2위",
-            "organization": "가톨릭대학교 컴퓨터정보공학부",
-            "date": "2017"
-        },
-        {
-            "title": "학습커뮤니티 우수 사례 선정",
-            "grade": "",
-            "organization": "가톨릭대학교",
-            "date": "2017"
-        },
-        {
-            "title": "학술제 창의소프트웨어 부문",
-            "grade": "2위 (개인)",
-            "organization": "가톨릭대학교 컴퓨터정보공학부",
-            "date": "2017"
-        },
-        {
-            "title": "대한민국 육군 특급전사 표창",
-            "grade": "",
-            "organization": "제 11기계화보병사단",
-            "date": "2016"
-        },
-        {
-            "title": "창업아이디어 공모전 인문사회 기술융합형",
-            "grade": "우수상",
-            "organization": "가톨릭대학교 창업대학",
-            "date": "2014"
-        },
-        {
-            "title": "학술제 창의소프트웨어 부문",
-            "grade": "2위 (개인)",
-            "organization": "가톨릭대학교 컴퓨터정보공학부",
-            "date": "2014"
-        },
-        {
-            "title": "동아리 활동 발표 대회",
-            "grade": "은상",
-            "organization": "전남고등학교",
-            "date": "2012"
-        },
-        {
-            "title": "과학, 정보 동아리 연구활동 발표대회",
-            "grade": "3위",
-            "organization": "전남고등학교",
-            "date": "2012"
-        },
-        {
-            "title": "과학탐구대회 로봇과학 부문",
-            "grade": "1위",
-            "organization": "광주동명중학교",
-            "date": "2010"
-        },
-        {
-            "title": "영재교육 창의적 산출물 발표대회",
-            "grade": "1위",
-            "organization": "광주광역시교육감",
-            "date": "2009"
-        },
-        {
-            "title": "광주학생명예헌장",
-            "grade": "자랑스런 광주학생 표창",
-            "organization": "2009",
-            "date": "광주광역시교육감"
-        },
-        {
-            "title": "대한민국 학생 창의력 올림피아드 광주 예선",
-            "grade": "1위",
-            "organization": "광주광역시교육감",
-            "date": "2009"
-        },
-        {
-            "title": "대한민국 학생 창의력 올림피아드 구조물 부문",
-            "grade": "1위",
-            "organization": "지식경제부장관",
-            "date": "2009"
-        },
-        {
-            "title": "대한민국 학생 창의력 올림피아드 즉석과제부문",
-            "grade": "1위",
-            "organization": "한국학교발명협회장",
-            "date": "2009"
-        }
-    ]
+    "award": []
 };
 
-function custonSort(a, b) {
+function dateSort(a, b) {
     if (a.date === b.date) {
         return 0
     }
     return a.date > b.date ? -1 : 1;
 }
 
-
 exports.getData = function () {
-    let projectList = [];
+    let projectList = [], awardList = [], careerList = [];
+
+
+    // 프로젝트
     fs.readdirSync(__dirname + '/../project').forEach(file => {
-        let data = require(__dirname + '/../project/' + file);
-        projectList.push(data);
+        projectList.push(require(__dirname + '/../project/' + file));
     });
-    projectList.sort(custonSort);
-    data.project = projectList;
+
+    // 수상
+    fs.readdirSync(__dirname + '/../award').forEach(file => {
+        awardList.push(require(__dirname + '/../award/' + file));
+    });
+
+    // 경력
+    fs.readdirSync(__dirname + '/../career').forEach(file => {
+        careerList.push(require(__dirname + '/../career/' + file));
+    });
+
+    data.project = projectList.sort(dateSort);
+    data.award = awardList.sort(dateSort);
+    data.career = careerList.sort(dateSort);
+
     return data;
 };
