1da33018058c05bddf12e452b44d0a359f53e077
duswnd25 <duswnd25@gmail.com>
2018-08-18 03:52:01 +0900

    필요없는 부분 삭제

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const compression = require("compression");
 const favicon = require("serve-favicon");
 const helmet = require("helmet");
-const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -24,9 +23,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 /** ================== ROUTER ================== **/
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use("/", require("./src/routes/main/main.js"));
 app.use("/kitchen", require("./src/routes/main/kitchen.js"));
 app.use("/api/kitchen/kakao", require("./src/routes/api/kakao"));
@@ -34,4 +33,5 @@ app.use("/api/kitchen/kakao", require("./src/routes/api/kakao"));
 app.get('*', function (req, res) {
     res.render('error');
 });
+
 module.exports = app;
