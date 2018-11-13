4839eb75a305f0b12c6aad6627726aa774ff5c5f
YeonJung Kim <duswnd25@gmail.com>
2018-11-14 02:45:45 +0900

    수정

--- config/config.global.js
+++ config/config.global.js
@@ -11,7 +11,5 @@ let config = {
 
 config.info.name = process.env.APP_NAME;
 config.info.env_type = 'production';
-config.pm2.max_memory = process.env.MAX_MEMORY;
-config.pm2.instance = process.env.INSTANCE;
 
 module.exports = config;
