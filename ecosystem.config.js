8a556785c3c8122a12154dc2296a63476cb14b15
YeonJung Kim <duswnd25@gmail.com>
2019-02-09 04:24:54 +0900

    pm2 test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: './bin/www',
     instances: 2,
     autorestart: true,
-    watch: true,
+    watch: false,
     max_memory_restart: '256M',
     env: {
       NODE_ENV: 'development'
